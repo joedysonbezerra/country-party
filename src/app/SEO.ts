e2760ba1bc2aa81e1b6e3e58e88856ca8086355f
joedysonbezerra <joedysonbezerra@gmail.com>
2024-06-09 11:47:25 -0300

    feat: added metadata

---
+++ src/app/SEO.ts
@@ -0,0 +1,27 @@
+export const SEO = {
+  title: "Lagoinha Caruaru | Festa Country",
+  description: "Faça sua inscrição para a festa country da Lagoinha Caruaru.",
+  keywords: ["igreja", "lagoinha"],
+  domain: "https://lagoinhacaruaru.com.br",
+  category: "church",
+  openGraph: {
+    title: "Lagoinha Caruaru | Festa Country",
+    description: "Faça sua inscrição para a festa country da Lagoinha Caruaru.",
+    url: "https://lagoinhacaruaru.com.br",
+    siteName: "Lagoinha Caruaru | Festa Country",
+    images: [
+      {
+        url: "https://lagoinhacaruaru.com.br/open-graph.jpg",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "pt-BR",
+    type: "website",
+  },
+  verification: {
+    other: {
+      me: ["suporte@lagoinhacaruaru.com.br", "https://lagoinhacaruaru.com.br"],
+    },
+  },
+};
