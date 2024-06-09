fa7b2f6f63cb26071025d45c7ca503454160992c
joedysonbezerra <joedysonbezerra@gmail.com>
2024-06-09 12:28:27 -0300

    improve form

--- src/app/(pages)/registration/page.tsx
+++ src/app/(pages)/registration/page.tsx
@@ -54,108 +54,110 @@ export default function Registration() {
   return (
     <>
       {isExploding && <Fireworks autorun={{ speed: 3 }} />}
-      <form onSubmit={handleSubmit}>
-        <div className="space-y-12 px-10">
-          <div className="border-b border-white/10 pb-12">
-            <h2 className="text-base font-semibold leading-7 text-white">
-              Preencha seus dados para garantir sua vaga!
-            </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-400">
-              Vamos utilizar essas informações somente para inscrição.
-            </p>
+      <div className="bg-white min-h-screen">
+        <form onSubmit={handleSubmit}>
+          <div className="space-y-12 px-10 bg-white">
+            <div className="border-b border-gray-300 pb-12">
+              <h2 className="text-base font-semibold leading-7 text-gray-800">
+                Preencha seus dados para garantir sua vaga!
+              </h2>
+              <p className="mt-1 text-sm leading-6 text-gray-600">
+                Vamos utilizar essas informações somente para inscrição.
+              </p>
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="first-name"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  Primeiro nome
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="firstName"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                  />
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-medium leading-6 text-gray-800"
+                  >
+                    Primeiro nome
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="firstName"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="pl-3 block w-full rounded-md bg-white py-1.5 text-gray-800 shadow-sm ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="last-name"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  Sobrenome
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="lastName"
-                    id="last-name"
-                    autoComplete="family-name"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                  />
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-medium leading-6 text-gray-800"
+                  >
+                    Sobrenome
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="lastName"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="pl-3 block w-full rounded-md bg-white py-1.5 text-gray-800 shadow-sm ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="phone"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  Telefone
-                </label>
-                <div className="mt-2">
-                  <InputMask
-                    mask="(99) 99999-9999"
-                    type="text"
-                    name="phone"
-                    id="phone"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                    value={formData.phone}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
 
-              {/* Age field */}
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="age"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  Idade
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="number"
-                    name="age"
-                    id="age"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                    value={formData.age}
-                    onChange={handleChange}
-                  />
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="phone"
+                    className="block text-sm font-medium leading-6 text-gray-800"
+                  >
+                    Telefone
+                  </label>
+                  <div className="mt-2">
+                    <InputMask
+                      mask="(99) 99999-9999"
+                      type="text"
+                      name="phone"
+                      id="phone"
+                      className="pl-3 block w-full rounded-md bg-white py-1.5 text-gray-800 shadow-sm ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                      value={formData.phone}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="age"
+                    className="block text-sm font-medium leading-6 text-gray-800"
+                  >
+                    Idade
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="number"
+                      name="age"
+                      id="age"
+                      className="pl-3 block w-full rounded-md bg-white py-1.5 text-gray-800 shadow-sm ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                      value={formData.age}
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="mt-6 flex items-center justify-end gap-x-6 px-10">
-          <button
-            type="submit"
-            className="rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-700"
-          >
-            Enviar sua inscrição
-          </button>
-        </div>
-      </form>
+          <div className="mt-6 flex items-center justify-end gap-x-6 px-10">
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-700"
+            >
+              Enviar sua inscrição
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
